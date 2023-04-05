8e2e50ce46ffff8f56b112649260af937c9b851e
Alabi Temitope <t.banji@rocketmail.com>
2023-04-05 13:47:29 +0100

    different components

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,9 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   template: `
-    <p>
+    <h2>
       contact works!
-    </p>
+    </h2>
+    <a routerLink="add"> Add Contact</a>
+    <br/>
+    <a routerLink="edit/1"> Edit Contact</a>
+
+    <div>
+      <router-outlet></router-outlet>
+    </div>
   `,
   styleUrls: ['./contact.component.css']
 })
