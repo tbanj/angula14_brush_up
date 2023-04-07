9bf25ab6d19e9edeec00b454d187a67c229ed658
Alabi Temitope <t.banji@rocketmail.com>
2023-04-07 03:00:31 +0100

    knowledge on binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,9 +9,19 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  headerName= "Welcome to Maja World"
-
+  headerName= "Welcome to Maja World";
+  salary =1000;
+  salaryPercent =0.35;
+  /* 
+  pipes is use with interpolation for data  formatting in .html file
+  pipes symbol is |
+  e.g lowercase, uppercase
+  */
   ngOnInit(): void {
+  }
+
+  FunctionClick(name: string): void {
+    alert(`Testing function ${name}`);
   }
 
 }
