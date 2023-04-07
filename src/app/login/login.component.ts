f4e7fab464713c2857d1b3ec9a11a434d975a62a
Alabi Temitope <t.banji@rocketmail.com>
2023-04-07 11:06:14 +0100

    login component implementation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from 'src/Material-Module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  /* 
+  since the login component is standalone we don't need to
+  import it at the app.module.ts file of the import section
+  
+  But if it's not standalone we need to import it into
+    app.module.ts file of the import section
+  */
+  imports: [CommonModule, MaterialModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
